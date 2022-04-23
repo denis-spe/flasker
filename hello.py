a07f99ced70e1e60c14cce742adc14f47a663a87
denis-spe <denisbrian07@gmail.com>
2022-04-24 01:03:11 +0200

    initial commit

---
+++ hello.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+
+# Create a Flask Instance
+app = Flask(__name__)
+
+# Filter!!!
+# safe
+# capitalize
+# lower
+# upper
+# title
+# trim
+# striptags
+
+
+# Create a route decorator
+@app.route('/')
+def index():
+    first_name = "John"
+    stuff = "this is <b style='font-size: 30px'>bold text</b>"
+
+    favourite_pizza = [
+        'Pepperoi', 
+        'cheese', 42,
+        "Mushroom"
+    ]
+    
+    return render_template(
+        "index.html", 
+        first_name=first_name,
+        stuff=stuff,
+        favourite_pizza=favourite_pizza 
+        )
+
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template("user.html", user_name=name)
+
+
+# create Custom error pages
+
+# Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+# Internet Server Error
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template("500.html"), 500
